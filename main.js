ef8c6777d52c7ec2b135aec5a164e4286ce0f504
Lisa Lemons - Grand Circus <LisaLemonsGrandCircus@Katies-MacBook-Air.local>
2019-02-07 22:06:43 -0500

    Updating with code we made in class

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // // Uncomment this entire file to see the javascript in action
+// // Make sure to pull up your dev tools to see the console logs
 // // The password is: secret
 // // Ctrl/Cmd + A to select the entire file
 // // Ctrl/Cmd + / to uncomment/comment the selection
@@ -8,38 +9,11 @@
 //     "Home",
 //     "About",
 //     "Blog",
-//     "Contact"
+//     "Contact",
+//     "Gallery"
 // ];
 
 // let navItemsContainer = document.getElementsByClassName('nav-items');
-
-// // for loop
-// for (i = 0; i < navItems.length; i++) {
-//     console.log(navItems[i]);
-// }
-
-// let name = undefined;
-
-// // while
-// while (!name) {
-//     name = prompt("What is your name?");
-// }
-
-// // do while
-// let userPass;
-// do {
-//     userPass = prompt("What is your password?");
-// } while (userPass !== "secret");
-
-// // for in
-// for (let navItem in navItems) {
-//     console.log(`For in navItem: ${navItem}`);
-// }
-
-// // for in
-// for (let container of navItemsContainer) {
-//     console.log(container);
-// }
 
 // // HTML Manipulation
 // // First we grab all containers with this class name
@@ -57,4 +31,67 @@
 //         li.appendChild(a); // We now append the <a> we made to the <li> we also made
 //         navItemContainer.appendChild(li); // Lastly, we append the elements we made to the containers we grabbed
 //     }
+// }
+
+// // FOR LOOPS
+// // Prints out all of the nav items from the Array declared at the top of this file
+// for (let i = 0; i < navItems.length; i++) {
+//     console.log(navItems[i]);
+// }
+
+// // Counts from 1 to 20 
+// for (let x = 1; x <= 20; x++) {
+//     console.log(x);
+// }
+
+// // Finds all even numbers between 1 - 20
+// for (let x = 1; x <= 20; x++) {
+//     if (x % 2 === 0) {
+//         console.log(x);
+//     }
+// }
+
+// // Another method to get all even numbers between 1 - 20
+// for (let x = 2; x <= 20; x+=2) {
+//     console.log(x);
+// }
+
+// let name = undefined;
+
+// // while
+// while (!name) {
+//     name = prompt("What is your name?");
+// }
+
+// // The same while loop from above in a for loop
+// for (name = undefined; !name;) {
+//     name = prompt("What is your name for my for loop?");
+// }
+
+// // do while 1
+// let userWantsToContinue = true;
+// let i = 0;
+// do {
+//   i++;
+//   console.log(i);
+//   userWantsToContinue = confirm("Do you want to continue?");
+// } while (userWantsToContinue);
+
+
+// // do while 2
+// let userPass;
+// do {
+//     userPass = prompt("What is your password?");
+// } while (userPass !== "secret");
+
+// // for in
+// // for properties IN objects
+// for (let navItem in navItems) {
+//     console.log(`For in navItem: ${navItem}`);
+// }
+
+// // for of
+// // for items OF arrays
+// for (let container of navItemsContainer) {
+//     console.log(container);
 // }
